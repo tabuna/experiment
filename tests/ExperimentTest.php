f65bdd194d1acd7a94bd6c536f1cdf1827d4b215
Alexandr Chernyaev <bliz48rus@gmail.com>
2020-01-13 12:25:07 +0300

    Apply fixes from StyleCI (#4)

--- tests/ExperimentTest.php
+++ tests/ExperimentTest.php
@@ -32,7 +32,7 @@ class ExperimentTest extends TestCase
     /**
      * @throws \Exception
      */
-    protected function setUp() : void
+    protected function setUp(): void
     {
         parent::setUp();
 
